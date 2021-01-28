b7c8dfe12819056421b46c2ce14d92532a2b7475
Ashwin Dutt <ashwinadutt@gmail.com>
2021-01-28 12:11:11 +1100

    Created project layout

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
+
+import useStyle from './styles'
+
+const Form = () => {
+    const classes = useStyles();
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Form
