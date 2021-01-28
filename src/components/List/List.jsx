7f22aa0d5fa0a7513acab9d09599975db806c046
Ashwin Dutt <ashwinadutt@gmail.com>
2021-01-28 16:58:01 +1100

    Added context api

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide, Icon} from '@material-ui/core'
-import {Delete, Money, MoneyOff } from '@material-ui/icons'
+import {Delete, MoneyOff } from '@material-ui/icons'
 
+import { ExpenseTrackerContext } from '../../context/context'
 import useStyles from './styles'
 
 const List = () => {
     const classes = useStyles();
 
-    const transactions = [{id: 1, type: 'Income', category: 'Salary', amount: 2000, date: new Date()}];
+    const { deleteTransaction } = useContext(ExpenseTrackerContext);
+
+    console.log(deleteTransaction)
+
+    const transactions = [
+        {id: 1, type: 'Income', category: 'Salary', amount: 5000, date: 'Wed Dec 16'},
+        {id: 2, type: 'Expense', category: 'Rent', amount: 1000, date: 'Thur Dec 17'},
+        {id: 3, type: 'Income', category: 'Salary', amount: 2000, date: 'Wed Dec 18'},
+    ];
 
     return (
         <MUIList dense={false} className={classes.list}>
